2e4215d3424b6f5e5a8676f5f8a8f8cf86ecb5cf
nickhsine <nickhsine97753017@gmail.com>
2016-04-12 16:17:54 +0800

    remove imageDecoractor.js

--- fields/types/html/entityDecorator.js
+++ fields/types/html/entityDecorator.js
@@ -2,7 +2,6 @@
 
 import { CompositeDecorator } from 'draft-js';
 import linkDecorator from './link/linkDecorator';
-// import imageDecorator from './image/imageDecorator';
 import React from 'react';
 
 const decorator = new CompositeDecorator([ linkDecorator]);
