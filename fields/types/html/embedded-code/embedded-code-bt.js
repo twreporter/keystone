415977dd549cdfc8a855fad4e3601efe9b44c431
nickhsine <nickhsine97753017@gmail.com>
2016-04-28 11:22:57 +0800

    Code refactor - add bt-wrapper.js, which is used to make button clickable and click to   show editing block

--- fields/types/html/embedded-code/embedded-code-bt.js
+++ fields/types/html/embedded-code/embedded-code-bt.js
@@ -1,46 +1,6 @@
 'use strict';
-import objectAssign from 'object-assign';
+import BtWrapper from '../base/bt-wrapper';
 import EmbeddedCodeEditingBlock from './embedded-code-editing-block';
 import React from 'react';
-import StyleButton from '../base/style-button';
 
-class EmbbedCodeBt extends StyleButton {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { active, shouldRenderEditingBlock } = this.state;
-
-        let Block = shouldRenderEditingBlock ? (
-            <EmbeddedCodeEditingBlock
-                {...this.props}
-                isModalOpen={true}
-                handleToggle={this.handleToggle}
-            />
-        ) : null;
-
-        let button = super.render();
-
-        return (
-          <div
-              className="Button Button--default"
-              style={{display: "inline-block"}}
-              onClick={this.handleToggle}
-              >
-              {Block}
-              {button}
-            </div>
-        );
-    }
-}
-
-EmbbedCodeBt.propTypes = objectAssign(StyleButton.propTypes, {
-    onToggle: React.PropTypes.func.isRequired,
-    embeddedCode: React.PropTypes.string
-});
-EmbbedCodeBt.defaultProps = objectAssign(StyleButton.defaultProps, {
-    embeddedCode: ''
-});
-
-export default EmbbedCodeBt;
+export default BtWrapper(EmbeddedCodeEditingBlock);
