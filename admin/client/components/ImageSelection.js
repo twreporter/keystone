383b1bbf570ff1cdf97540082ec66c5d16898f22
nickhsine <nickhsine97753017@gmail.com>
2016-06-05 10:30:56 +0800

    extract common funcs in ImageSelection as SelectionMixin

--- admin/client/components/ImageSelection.js
+++ admin/client/components/ImageSelection.js
@@ -1,84 +1,50 @@
 'use strict';
 import { ImageGrid } from './ImageGrid';
-import { shallowEqual } from 'react-pure-render';
+import _ from 'lodash';
+import SelectionMixin from './mixins/SelectionMixin';
 import React from 'react';
 
-const ImageSelection = React.createClass({
-	displayName: 'ImageSelection',
-	propTypes: {
-        images: React.PropTypes.array,
-        selectedImages: React.PropTypes.array,
-        selectionLimit: React.PropTypes.number,
-        updateSelection: React.PropTypes.func.isRequired
-	},
+class ImageSelection extends SelectionMixin(React.Component) {
+    constructor (props) {
+        super(props);
 
-	getDefaultProps () {
-		return {
-            images: [],
-			selectedImages: [],
-            selectionLimit: 1
-		};
-	},
-
-	getInitialState () {
-		return {
-            images: Array.isArray(this.props.images) ? this.props.images : [],
-            selectedImages: Array.isArray(this.props.selectedImages) ? this.props.selectedImages : []
-		};
-	},
+        this.state = {
+            items: props.images,
+            selectedItems: props.selectedImages
+        }
+    }
 
     componentWillReceiveProps (nextProps) {
-        this.setState({
-            images: nextProps.images,
-            selectedImages: nextProps.selectedImages
+        let props = {};
+        _.merge(props, {
+            items: nextProps.images,
+            selectedItems: nextProps.selectedImages
         });
-    },
-
-    shouldComponentUpdate (nextProps, nextState) {
-        let shouldUpdate = false;
-        if ( this.props.updateSelection !== nextProps.updateSelection ||
-            this.props.selectionLimit !== nextProps.selectionLimit ) {
-                shouldUpdate = true;
-            }
-        return shouldUpdate || !shallowEqual(this.state, nextState);
-    },
-
-    handleClick (image) {
-        let _selectImages = this.state.selectedImages;
-        let filtered = _selectImages.filter((selectedImage) => {
-            return selectedImage.id !== image.id;
-        });
-
-        // select the image
-        if (filtered.length === _selectImages.length) {
-            // select many
-            if (this.props.selectionLimit > 1) {
-                const len = filtered.length;
-                if (len >= this.props.selectionLimit) {
-                    filtered.shift();
-                }
-                // create a new array for pure render
-                filtered = len === 0 ? [image] : filtered.concat(image);
-            } else {
-                filtered = [image];
-            }
-        }
-        this.setState({
-            selectedImages: filtered
-        }, () => {
-            this.props.updateSelection(filtered);
-        });
-    },
+        super.componentWillReceiveProps(props);
+    }
 
 	render () {
 		return (
             <ImageGrid
-                images={this.state.images}
-                onClick={this.handleClick}
-                selectedImages={this.state.selectedImages}
+                images={this.state.items}
+                onSelect={this.handleSelecet}
+                selectedImages={this.state.selectedItems}
             />
 		);
 	}
-});
+};
+
+ImageSelection.propTypes = {
+    images: React.PropTypes.array,
+    selectedImages: React.PropTypes.array,
+    selectionLimit: React.PropTypes.number,
+    updateSelection: React.PropTypes.func.isRequired
+};
+
+ImageSelection.defaultProps = {
+    images: [],
+    selectedImages: [],
+    selectionLimit: 1
+};
 
 module.exports = ImageSelection;
