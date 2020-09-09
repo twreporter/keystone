d039cb6d24b16a79d9c88636cb2f9cbac7718f39
nickhsine <nickhsine97753017@gmail.com>
2020-09-09 14:40:50 +0800

    refactor: fix indentation

--- fields/types/preview/PreviewColumn.js
+++ fields/types/preview/PreviewColumn.js
@@ -3,26 +3,26 @@ import ItemsTableCell from '../../../admin/client/components/ItemsTableCell';
 import ItemsTableValue from '../../../admin/client/components/ItemsTableValue';
 
 var PreviewColumn = React.createClass({
-	displayName: 'PreviewColumn',
-	propTypes: {
-		col: React.PropTypes.object,
-		data: React.PropTypes.object,
-	},
-	renderValue () {
+  displayName: 'PreviewColumn',
+  propTypes: {
+    col: React.PropTypes.object,
+    data: React.PropTypes.object,
+  },
+  renderValue () {
     const previewOrigin = Keystone.previewOrigin || 'https://keystone-preview.twreporter.org'
-		return (
-			<ItemsTableValue truncate={false} field={this.props.col.type}>
+    return (
+      <ItemsTableValue truncate={false} field={this.props.col.type}>
         <a style={{ textDecoration: 'underline' }} href={`${previewOrigin}/a/${this.props.data.slug}`} target="_blank" >{this.props.col.label}</a>
-			</ItemsTableValue>
-		);
-	},
-	render () {
-		return (
-			<ItemsTableCell>
-				{this.renderValue()}
-			</ItemsTableCell>
-		);
-	},
+      </ItemsTableValue>
+    );
+  },
+  render () {
+    return (
+      <ItemsTableCell>
+        {this.renderValue()}
+      </ItemsTableCell>
+    );
+  },
 });
 
 module.exports = PreviewColumn;
--- fields/types/preview/PreviewField.js
+++ fields/types/preview/PreviewField.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { Button, FormField } from 'elemental';
 
 module.exports = Field.create({
-	displayName: 'PreviewField',
-	renderUI () {
+  displayName: 'PreviewField',
+  renderUI () {
     const previewOrigin = Keystone.previewOrigin || 'https://keystone-preview.twreporter.org'
     let url = `${previewOrigin}/a/${this.props.values.name}`;
-		return (
-			<FormField className="field-type-preview">
-				<Button type="default"><a href={url} target="_blank">{this.props.label}</a></Button>
-			</FormField>
-		);
-	},
+    return (
+      <FormField className="field-type-preview">
+        <Button type="default"><a href={url} target="_blank">{this.props.label}</a></Button>
+      </FormField>
+    );
+  },
 });
