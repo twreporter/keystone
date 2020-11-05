b51d57e1e54789148d2254f936bb6b729e3af95f
nickhsine <nickhsine97753017@gmail.com>
2020-11-05 12:10:14 +0800

    refactor: update GcsImageFilter.js
    This patch copies `CloudinaryFilter.js` source code
    to `GcsImageFilter.js`.

--- fields/types/gcsimage/GcsImageFilter.js
+++ fields/types/gcsimage/GcsImageFilter.js
@@ -1,1 +1,34 @@
-module.exports = require('../cloudinaryimage/CloudinaryImageFilter');
+import React from 'react';
+
+import { SegmentedControl } from 'elemental';
+
+var PasswordFilter = React.createClass({
+
+  getInitialState () {
+    return {
+      checked: this.props.value || true,
+    };
+  },
+
+  toggleChecked (checked) {
+    this.setState({
+      checked: checked,
+    });
+  },
+
+  renderToggle () {
+    let options = [
+      { label: 'Is Set', value: true },
+      { label: 'Is NOT Set', value: false },
+    ];
+
+    return <SegmentedControl equalWidthSegments options={options} value={this.state.checked} onChange={this.toggleChecked} />;
+  },
+
+  render () {
+    return this.renderToggle();
+  },
+
+});
+
+module.exports = PasswordFilter;
