9bdcc9641766d9a69e57eadae3c50e903812eaa6
nickhsine <nickhsine97753017@gmail.com>
2020-11-05 12:02:36 +0800

    refactor: update GcsImageColumn.js
    This patch display `resizedTargets.mobile.url` first.
    If mobile url is not existed, then display gcs file location.

--- fields/types/gcsimage/GcsImageColumn.js
+++ fields/types/gcsimage/GcsImageColumn.js
@@ -8,11 +8,28 @@ const _ = {
 var GcsImageColumn = React.createClass({
 	render: function () {
 		var value = this.props.data.fields[this.props.col.path] || {};
-		var isVal = _.get(value, ['resizedTargets', 'mobile', 'url'], value.url);
+		const thumbnail = _.get(value, ['resizedTargets', 'mobile', 'url']);
+    const gcsDir = _.get(value, 'gcsDir', '');
+    const gcsBucket = _.get(value, 'gcsBucket', '');
+    const filename = _.get(value, 'filename', '');
+    const location = gcsDir ? `gs://${gcsBucket}/${gcsDir}/${filename}` : `gs://${gcsBucket}/${filename}`;
+
+    const displayJsx = thumbnail ? (
+      <a href={thumbnail} target="_blank">
+        <img src={thumbnail} height="150" width="150" />
+      </a>
+    ) : (
+      <p>
+        {location}
+      </p>
+    )
+
 		return (
-			<td className="ItemList__col">
-				<div className="ItemList__value ItemList__value--gcs-image"><a href={isVal} target="_blank"><img src={isVal} height="150" width="150"/></a></div>
-			</td>
+      <td className="ItemList__col">
+        <div className="ItemList__value ItemList__value--gcs-image">
+          {displayJsx}
+        </div>
+      </td>
 		);
 	},
 });
