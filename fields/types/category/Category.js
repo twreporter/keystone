faa6ff9c5e10c00d4d904837c62cfaee000c6953
duidae <duidae@gmail.com>
2022-07-26 15:36:03 +0800

    fix: lint

--- fields/types/category/Category.js
+++ fields/types/category/Category.js
@@ -114,9 +114,9 @@ module.exports = Field.create({
       return value.map((categorySet, index) => {
         return (
           <div key={`categorySet-${index}`} style={categorySetStyle}>
-            {index > 0 ?
-              <button type="button" className="ItemList__control ItemList__control--delete-no-focus" onClick={() => this.onRemoveCategorySet(index)}><span className={'octicon octicon-trashcan'} /></button> :
-              <div className="ItemList__control ItemList__control--delete-no-focus" />
+            {index > 0
+              ? <button type="button" className="ItemList__control ItemList__control--delete-no-focus" onClick={() => this.onRemoveCategorySet(index)}><span className={'octicon octicon-trashcan'} /></button>
+              : <div className="ItemList__control ItemList__control--delete-no-focus" />
             }
             <div style={majorMenuStyle}><Select placeholder="分類" options={majorCategoryOptions} value={categorySet.major} onChange={(selected) => this.onUpdateCategorySet(index, { major: selected.value, sub: '' })} /></div>
             <div style={menuStyle}><Select placeholder="子分類" options={subCategoryOptions} value={categorySet.sub} onChange={(selected) => this.onUpdateSubCategory(index, selected.value)} /></div>
