dc91a0f818f241bb0f2d70aca327170755f23078
duidae <duidae@gmail.com>
2022-08-18 13:50:10 +0800

    fix: fix options issue

--- fields/types/categorySet/CategorySet.js
+++ fields/types/categorySet/CategorySet.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Select from 'react-select';
 import xhr from 'xhr';
+import async from 'async';
 
 import Field from '../Field';
 
@@ -30,69 +31,63 @@ const btnStyle = {
   backgroundColor: 'transparent'
 };
 
-// TODO: remove this when integrate with db
-const categoryMap = new Map([
-  ['國際兩岸', ['香港', '中國', '美國', '日韓', '東南亞', '歐洲', '其他']],
-  ['人權司法', ['性別', '勞動', '移工與移民', '居住正義', '轉型正義', '精神疾病', '司法改革', '數位人權']],
-  ['政治社會', ['選舉', '政黨與地方政治', '政策', '交通', '食安', '資訊戰', '社會觀察']],
-  ['醫療健康', ['疫情', '醫療政策', '生物科技', '病人自主權利', '心理', '長照', '公共衛生']],
-  ['環境永續', ['能源', '海洋', '山林', '動物', '環境污染', '自然災害']],
-  ['經濟產業', ['經濟', '能源', '農業', '科技']],
-  ['文化生活', ['體育', '電影', '文學', '音樂', '戲劇', '藝術']],
-  ['教育校園', ['教育政策', '高等教育', '青少年', '育兒', '少子化']],
-]);
-const categoryOptions = Array.from(categoryMap.keys()).map(category => { return { value: category, label: category }; });
-
 module.exports = Field.create({
 
   displayName: 'RelationshipField',
 
   getInitialState() {
-    // TODO: init this.state.value with this.props.value
     return {
-      value: [{ category: '國際兩岸', subcategory: '香港' }],
+      categoryOptions: null,
+      subcategoryOptionsMap: null,
+      // value: this.props.value
+      value: [{ category: '62fb9e38aa35b19083567799', subcategory: '5773c3c8c107b51000e9beb1' }],
     };
   },
 
   componentDidMount() {
-    this.categoryOptions = [];
-    this.subcategoryOptionsMap = new Map();
-    this.loadOptions();
-  },
-
-  loadOptions() {
     xhr({
       url: Keystone.adminPath + '/api/post-categories?basic',
       responseType: 'json',
     }, (err, resp, data) => {
-      if (err || !data || !data.results) {
+      if (err || !data || !Array.isArray(data.results)) {
         console.error('Error loading items:', err);
         return;
       }
-      data.results.forEach(category => {
-        // TODO: how to get subcategory field from query?
-        if (category && category.id && category.name && category.subcategory) {
-          this.categoryOptions.push({ value: category.id, label: category.name });
-          /* TODO: can we fetch subcategory just once?
-          let subcategoryOptions = [];
-          if (Array.isArray(category.subcategory)) {
-            category.subcategory.forEach(subcategoryId => {
-              xhr({
-                url: Keystone.adminPath + `/api/tags?basic&search=${subcategoryId}`,
-                responseType: 'json',
-              }, (err, resp, data) => {
-                if (err || !data || !data.results) {
-                  console.error('Error loading items:', err);
-                  return;
-                }
-                subcategoryOptions.push({value: subcategoryId, label: data.name}});
+      let categoryOptions = [];
+      let subcategoryOptionsMap = new Map([]);
+      async.each(data.results, (category, callback) => {
+        if (category && category.id && category.name && category.fields && Array.isArray(category.fields.subcategory) && category.fields.subcategory.length > 0) {
+          categoryOptions.push({ value: category.id, label: category.name });
+          const subcategoryIDs = category.fields.subcategory;
+          async.map(subcategoryIDs, (subcategoryID, callback) => {
+            xhr({
+              url: Keystone.adminPath + `/api/tags?basic&search=${subcategoryID}`,
+              responseType: 'json',
+            }, (err, resp, data) => {
+              if (err || !data || !Array.isArray(data.results) || data.results.length <= 0) {
+                console.error('Error loading items:', err);
+                return callback(err);
+              }
+              callback(null, { value: data.results[0].id, label: data.results[0].name });
             });
-          }
-          this.subcategoryOptionsMap.set(category.id, subcategoryOptions);
-          */
+          }, (err, results) => {
+            if (err) {
+              console.log(err);
+            }
+            subcategoryOptionsMap.set(category.id, err ? [] : results);
+            callback(null);
+          });
+        } else {
+          callback(null);
+        }
+      }, err => {
+        if (err) {
+          console.log(err);
+        } else {
+          // eslint-disable-next-line react/no-did-mount-set-state
+          this.setState({ categoryOptions: categoryOptions, subcategoryOptionsMap: subcategoryOptionsMap });
         }
       });
-      console.log('this.categoryOptions', this.categoryOptions, 'this.subcategoryOptions', this.subcategoryOptions);
     });
   },
 
@@ -127,20 +122,17 @@ module.exports = Field.create({
   },
 
   renderCategorySetSelect() {
-    const { value } = this.state;
+    const { value, categoryOptions, subcategoryOptionsMap } = this.state;
     if (Array.isArray(value)) {
       return value.map((categorySet, index) => {
-        let subcategoryOptions = categoryMap.get(categorySet.category);
-        if (Array.isArray(subcategoryOptions)) {
-          subcategoryOptions = subcategoryOptions.map(subcategory => { return { value: subcategory, label: subcategory }; });
-        }
+        const subcategoryOptions = subcategoryOptionsMap ? subcategoryOptionsMap.get(categorySet.category) : [];
         return (
           <div key={`categorySet-${index}`} style={categorySetStyle}>
             {index > 0
               ? <button type="button" className="ItemList__control ItemList__control--delete-no-focus" onClick={() => this.onRemoveCategorySet(index)}><span className={'octicon octicon-trashcan'} /></button>
               : <div className="ItemList__control ItemList__control--delete-no-focus" />
             }
-            <div style={categoryMenuStyle}><Select placeholder="分類" clearable={false} options={categoryOptions} value={categorySet.category} onChange={(selected) => this.onUpdateCategorySet(index, { category: selected.value, subcategory: '' })} /></div>
+            <div style={categoryMenuStyle}><Select placeholder="分類" disabled={!categoryOptions} clearable={false} options={categoryOptions} value={categorySet.category} onChange={(selected) => this.onUpdateCategorySet(index, { category: selected.value, subcategory: '' })} /></div>
             <div style={subcategoryMenuStyle}><Select placeholder="子分類" disabled={!categorySet.category || !subcategoryOptions} clearable={false} options={subcategoryOptions} value={categorySet.subcategory} onChange={(selected) => this.onUpdateSubcategory(index, selected.value)} /></div>
           </div>
         );
@@ -154,12 +146,7 @@ module.exports = Field.create({
       <div>
         {this.renderCategorySetSelect()}
         <div style={btnContainerStyle}><button type="button" style={btnStyle} onClick={this.onAddCategorySet}>新增分類</button></div>
-        {<input type="hidden" name={this.props.path} value={JSON.stringify([ // TODO
-          { category: '57175d923970a5e46ff854db', subcategory: '57175d923970a5e46ff854db' },
-          { category: '57175d923970a5e46ff854db', subcategory: '57175d923970a5e46ff854db' },
-        ])} />
-        }
-        {/* <input type="hidden" name={this.props.path} value={JSON.stringify(this.state.value)}/>*/}
+        <input type="hidden" name={this.props.path} value={JSON.stringify(this.state.value)}/>
       </div>
     );
   },
