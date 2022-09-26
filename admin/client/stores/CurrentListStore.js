2c918d69a607d16dd95820d0542be9f696ba98a3
duidae <duidae@gmail.com>
2022-09-26 16:40:08 +0800

    feat: enable pre filter

--- admin/client/stores/CurrentListStore.js
+++ admin/client/stores/CurrentListStore.js
@@ -173,6 +173,11 @@ const CurrentListStore = new Store({
   loadItems(options = {}) {
     _loading = true;
     defaultRowAlert();
+
+    if (_list.preFilter) {
+      active.filters.push(_list.preFilter);
+    }
+
     _list.loadItems({
       search: active.search,
       filters: active.filters,
